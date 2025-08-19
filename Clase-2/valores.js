a96508a4c3d64807bf9c7c2c4299b60151cc75b8
Matias <matias.duro@ina.edu.ar>
2025-08-19 20:19:08 -0300

    primer commit

---
+++ Clase-2/valores.js
@@ -0,0 +1,56 @@
+// Tipos de valores 
+
+let nombre = "matias" // tipo: String
+let edad = 30 // tipo: number
+let decimales = 3.14 // tipo: number
+let activo = true
+let noDefinida
+let nulo = null
+let materias = ["PNT2", "TP2"]
+
+
+// TIP:
+// Ctrl + D, habiendo seleccionado una palabra, te permite manipular ambas a la vez
+
+// Alt + click, permite escribir en varias lineas
+// console.log("nombre",typeof nombre)
+// console.log("edad",typeof edad)
+// console.log("losBooleanos",typeof losBooleanos);
+// console.log("decimales",typeof decimales)
+// console.log("noDefinida",typeof noDefinida)
+// console.log("nulo",typeof nulo)
+// console.log("arrays",typeof arrays)
+
+
+let arrayUsuario = ["Matias", "Duro", "Profesor", "PNT2"]
+
+// console.log("arrays usuario", arrayUsuario[2])
+
+
+let usuario = {
+    // Llave : Valor
+    nombre: nombre,
+    apellido: "Duro",
+    estado: activo,
+    materias: materias
+}
+
+// console.log("Usuario: ", usuario.materias.length);
+
+let alumno = "Mateo"
+
+let age = 25
+
+console.log("Alumno es Mateo y tiene 25 de edad");
+
+
+console.log("Alumno es " + alumno + " y tiene " + edad + " de edad");
+
+console.log(`Alumno es ${alumno} y tiene ${edad} de edad`);
+
+
+
+
+
+
+
--- Clase-2/variables.js
+++ Clase-2/variables.js
@@ -1,1 +1,48 @@
-// 
+// TIP:
+// Para comentar y descomentar rapidamente, es ctrl + / 
+// Teclado espanol: es ctrl + shift + 7
+
+// var no se re comienda en codigo moderno
+// var es un tipo de variable, que se puede redeclarar, y reasignar
+var nombre = 'Matias'
+
+// console.log("LOG VariableNombre:", nombre);
+
+nombre = 'Lucas'
+
+// console.log("LOG VariableNombre:", nombre);
+
+var nombre = 'Lautaro'
+
+// console.log("LOG VariableNombre:", nombre);
+
+
+// let, se puede reasignar, pero no redeclarar, en el mismo scope 
+
+let edad = 20;
+
+// console.log("Edad con let: ", edad);
+
+if(edad > 19){
+    // let edad = 25
+
+    // console.log("edad es :", edad);
+    
+}
+
+// console.log("Edad con let: ", edad);
+
+
+// const, no se puede reasignar, ni reclarar
+
+
+const materias = ["PNT2", "TP2"]
+
+console.log("Materias: ", materias);
+
+if(true){
+    materias = ["TP2"]
+
+}
+
+console.log("Materias: ", materias);
