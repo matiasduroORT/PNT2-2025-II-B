b95c14768c174c044b38197361d008f787c20c40
Matias <matias.duro@ort.edu.ar>
2025-09-17 18:07:10 -0300

    clase 6

--- Clase-5/src/App.jsx
+++ Clase-5/src/App.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import Home from './Home'
 import { TodoApp } from './components/TodoApp/TodoApp'
 
 function App() {
-
   const tareasIniciales = [
     {id: 1, titulo: "tarea 1", completada: true},
     {id: 2, titulo: "tarea 2", completada: false},
   ]
 
   const [tareas, setTareas] = useState(tareasIniciales)
-
   const [nuevaTarea, setNuevaTarea] = useState("")
+
+
 
 
   const handleChange = (e) => {    
@@ -45,6 +45,19 @@ function App() {
   const handleDelete = (id) =>{
     setTareas(tareas.filter((t) => t.id !== id))
   }
+
+  useEffect(() => {
+    // first en el first, va toda la logica, 
+    // que quiero que se ejecute, cuando el componente, se monta, o hace update
+    console.log("Carga App")
+
+    // return () => {
+    //   second     El return, se ejecuta, cuando el componente se desmonta 
+    // }
+  }, [tareas]) // en el [ ] van las props o estados, que van a provocar que el componente 
+              // se renderize nuevamente 
+              // si el array de dependencias, esta vacio, este use effect solo se ejecuta
+              //  cuando el componente se monta
   
 
   return (
@@ -62,7 +75,8 @@ function App() {
     <button onClick={agregarTarea}>Agregar Tarea</button>
 
     <ul>
-      {tareas.map((tarea) => (
+      {
+      tareas.map((tarea) => (
         <li key={tarea.id} style={{margin: "8px 0", textDecoration: tarea.completada ? "line-through" : "none"}}>
           {tarea.titulo}
 
@@ -74,7 +88,8 @@ function App() {
             Eliminar Tarea
           </button>
         </li>
-      ))}
+      ))
+      }
     </ul>
    </>
   )
